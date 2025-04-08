99032f3018d1a70f4e5af7c4e46f3cf1cafa4ba6
ShawnWang <49603488+ShawnWang04@users.noreply.github.com>
2025-04-08 21:27:53 +0800

    Create HiLo-AIFI.py
    HiLo-AIFI

---
+++ HiLo-AIFI.py
@@ -0,0 +1,136 @@
+class HiLo-AIFI(nn.Module):
+    """Defines a single layer of the transformer encoder."""
+
+    def __init__(self, c1, cm=2048, num_heads=8, dropout=0.0, act=nn.GELU(), normalize_before=False):
+        """Initialize the TransformerEncoderLayer with specified parameters."""
+        super().__init__()
+        self.HiLo = HiLo(c1)
+        # Implementation of Feedforward model
+        self.fc1 = nn.Conv2d(c1, cm, 1)
+        self.fc2 = nn.Conv2d(cm, c1, 1)
+
+        self.norm1 = LayerNorm(c1)
+        self.norm2 = LayerNorm(c1)
+        self.dropout = nn.Dropout(dropout)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.act = act
+        self.normalize_before = normalize_before
+
+    def forward_post(self, src, src_mask=None, src_key_padding_mask=None, pos=None):
+        """Performs forward pass with post-normalization."""
+        src2 = self.HiLo(src)
+        src = src + self.dropout1(src2)
+        src = self.norm1(src)
+        src2 = self.fc2(self.dropout(self.act(self.fc1(src))))
+        src = src + self.dropout2(src2)
+        return self.norm2(src)
+
+    def forward(self, src, src_mask=None, src_key_padding_mask=None, pos=None):
+        """Forward propagates the input through the encoder module."""
+        return self.forward_post(src, src_mask, src_key_padding_mask, pos)
+
+class HiLo(nn.Module):
+    """
+    HiLo Attention
+
+    Link: https://arxiv.org/abs/2205.13213
+    """
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0., window_size=2, alpha=0.5):
+        super().__init__()
+        assert dim % num_heads == 0, f"dim {dim} should be divided by num_heads {num_heads}."
+        head_dim = int(dim/num_heads)
+        self.dim = dim
+
+        # self-attention heads in Lo-Fi
+        self.l_heads = int(num_heads * alpha)
+        # token dimension in Lo-Fi
+        self.l_dim = self.l_heads * head_dim
+
+        # self-attention heads in Hi-Fi
+        self.h_heads = num_heads - self.l_heads
+        # token dimension in Hi-Fi
+        self.h_dim = self.h_heads * head_dim
+
+        # local window size. The `s` in our paper.
+        self.ws = window_size
+
+        if self.ws == 1:
+            # ws == 1 is equal to a standard multi-head self-attention
+            self.h_heads = 0
+            self.h_dim = 0
+            self.l_heads = num_heads
+            self.l_dim = dim
+
+        self.scale = qk_scale or head_dim ** -0.5
+
+        # Low frequence attention (Lo-Fi)
+        if self.l_heads > 0:
+            if self.ws != 1:
+                self.sr = nn.AvgPool2d(kernel_size=window_size, stride=window_size)
+            self.l_q = nn.Linear(self.dim, self.l_dim, bias=qkv_bias)
+            self.l_kv = nn.Linear(self.dim, self.l_dim * 2, bias=qkv_bias)
+            self.l_proj = nn.Linear(self.l_dim, self.l_dim)
+
+        # High frequence attention (Hi-Fi)
+        if self.h_heads > 0:
+            self.h_qkv = nn.Linear(self.dim, self.h_dim * 3, bias=qkv_bias)
+            self.h_proj = nn.Linear(self.h_dim, self.h_dim)
+
+    def hifi(self, x):
+        B, H, W, C = x.shape
+        h_group, w_group = H // self.ws, W // self.ws
+
+        total_groups = h_group * w_group
+
+        x = x.reshape(B, h_group, self.ws, w_group, self.ws, C).transpose(2, 3)
+
+        qkv = self.h_qkv(x).reshape(B, total_groups, -1, 3, self.h_heads, self.h_dim // self.h_heads).permute(3, 0, 1, 4, 2, 5)
+        q, k, v = qkv[0], qkv[1], qkv[2]  # B, hw, n_head, ws*ws, head_dim
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale  # B, hw, n_head, ws*ws, ws*ws
+        attn = attn.softmax(dim=-1)
+        attn = (attn @ v).transpose(2, 3).reshape(B, h_group, w_group, self.ws, self.ws, self.h_dim)
+        x = attn.transpose(2, 3).reshape(B, h_group * self.ws, w_group * self.ws, self.h_dim)
+
+        x = self.h_proj(x)
+        return x
+
+    def lofi(self, x):
+        B, H, W, C = x.shape
+
+        q = self.l_q(x).reshape(B, H * W, self.l_heads, self.l_dim // self.l_heads).permute(0, 2, 1, 3)
+
+        if self.ws > 1:
+            x_ = x.permute(0, 3, 1, 2)
+            x_ = self.sr(x_).reshape(B, C, -1).permute(0, 2, 1)
+            kv = self.l_kv(x_).reshape(B, -1, 2, self.l_heads, self.l_dim // self.l_heads).permute(2, 0, 3, 1, 4)
+        else:
+            kv = self.l_kv(x).reshape(B, -1, 2, self.l_heads, self.l_dim // self.l_heads).permute(2, 0, 3, 1, 4)
+        k, v = kv[0], kv[1]
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, H, W, self.l_dim)
+        x = self.l_proj(x)
+        return x
+
+    def forward(self, x):
+        x = x.permute(0, 2, 3, 1)
+
+        if self.h_heads == 0:
+            x = self.lofi(x)
+            return x.permute(0, 3, 1, 2)
+
+        if self.l_heads == 0:
+            x = self.hifi(x)
+            return x.permute(0, 3, 1, 2)
+
+        hifi_out = self.hifi(x)
+        lofi_out = self.lofi(x)
+
+        x = torch.cat((hifi_out, lofi_out), dim=-1)
+        x = x.permute(0, 3, 1, 2)
+        return x
