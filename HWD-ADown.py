3e5b34c7048662666307151e48b5f24443da5075
ShawnWang <49603488+ShawnWang04@users.noreply.github.com>
2025-04-08 21:23:53 +0800

    Create HWD-ADown.py
    HWD-ADown

---
+++ HWD-ADown.py
@@ -0,0 +1,32 @@
+class HWD_ADown(nn.Module):
+    def __init__(self, c1, c2):  # ch_in, ch_out, shortcut, kernels, groups, expand
+        super().__init__()
+        self.c = c2 // 2
+        # self.cv1 = Conv(c1 // 2, self.c, 3, 2, 1)
+        self.cv1 = HWD(c1 // 2, self.c, 3, 1, 1)
+        self.cv2 = Conv(c1 // 2, self.c, 1, 1, 0)
+
+    def forward(self, x):
+        x = nn.functional.avg_pool2d(x, 2, 1, 0, False, True)
+        x1, x2 = x.chunk(2, 1)
+        x1 = self.cv1(x1)
+        x2 = torch.nn.functional.max_pool2d(x2, 3, 2, 1)
+        x2 = self.cv2(x2)
+        return torch.cat((x1, x2), 1)
+
+
+class HWD(nn.Module):
+    def __init__(self, in_ch, out_ch, k, s, p):
+        super(HWD, self).__init__()
+        from pytorch_wavelets import DWTForward
+        self.wt = DWTForward(J=1, mode='zero', wave='haar')
+        self.conv = Conv(in_ch * 4, out_ch, k, s, p)
+
+    def forward(self, x):
+        yL, yH = self.wt(x)
+        y_HL = yH[0][:, :, 0, ::]
+        y_LH = yH[0][:, :, 1, ::]
+        y_HH = yH[0][:, :, 2, ::]
+        x = torch.cat([yL, y_HL, y_LH, y_HH], dim=1)
+        x = self.conv(x)
+        return x
